8e135eb73ba9f5e826524bd657a9e20e48f71fe4
JamesHenderson627 <jhenderson627@gmail.com>
2015-09-16 22:05:43 -0500

    Added color functions

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ window.onload = function() {
 
 	var newTime = document.getElementById("clock"),
 		hexColor = document.getElementById("container"),
+		hexTime = document.getElementById("hex"),
 		bar = document.getElementById("progressBar"),
 		width = new Date().getSeconds() * 5;
 
@@ -19,24 +20,42 @@ window.onload = function() {
 		newTime.innerHTML = clock;
 	}
 
-	var changeHex = function() {
+	function hexToTime() {
+		var date = new Date(),
+			hexSecond = toStr(date.getSeconds() * 255/60).toUpperCase(),
+			hexMinute = toStr(date.getMinutes() * 255/60).toUpperCase(),
+			hexHour = toStr((date.getHours() * 255/24)-3).toUpperCase()
 
+		hexTime.innerHTML = "<p>" + hexHour + ":" + hexMinute + ":" + hexSecond + "</p>";
+
+		var colors = '#' + hexHour + hexMinute + hexSecond;
+		
+	    hexColor.style.background = "-webkit-radial-gradient(center, circle cover, #FFFFE6," + colors + ")";
+	}
+
+	function add0(num) {
+	    var dig = num + "";
+	    if (dig.length < 2) dig = "0" + dig;
+	    return dig;
+	}
+
+	function toStr(num){
+		return add0((Math.round(num)).toString(16));
 	}
 
 	var changeBar = function() {
 		if (width <= 300) width += 5;
 		if (width > 300) width = 0;
 
-		bar.style.width = width + 'px';
+		bar.style.width = width + "px";
 		return bar.style.width;
 	}
 
 	var updateView = function() {
 		makeClock();
 		changeBar();
+		hexToTime();
 	}
-
-	updateView()
 
 	setInterval(updateView, 1000)
 
